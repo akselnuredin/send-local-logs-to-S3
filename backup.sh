cc282e95ef8fd868772f8fef8a96d1f8abb46292
akselnuredin <akslnrdn@gmail.com>
2021-10-12 10:17:36 +0200

    Bactest word renamed

--- backup.sh
+++ backup.sh
@@ -7,7 +7,7 @@ MONTH=$(date +"%m")
 HOSTNAME=$(hostname)
 CUSTOM=$(date +"%F")
 LOG_LOCATION='/opt/app/arkcase/log'
-LOG_FILE='$LOG_LOCATION/bactest-$HOSTNAME.tar.gz'
+LOG_FILE='$LOG_LOCATION/bactes-$HOSTNAME.tar.gz'
 DEST_LOCATION='/sharecare-prod-s3fs/'
 
 
@@ -16,17 +16,16 @@ if [[ ! -d /opt/$YEAR ]] || [[ ! -d /opt/$YEAR/$MONTH ]]; then
     mkdir -p /opt/$YEAR/$MONTH;
 fi;
 
-mv  $LOG_LOCATION/bactest-$CUSTOM.log $LOG_LOCATION/bactest-$HOSTNAME.log
+mv  $LOG_LOCATION/bactes-$CUSTOM.log $LOG_LOCATION/bactes-$HOSTNAME.log
 
-tar -czvf $LOG_LOCATION/bactest-$HOSTNAME.tar.gz $LOG_LOCATION/bactest-$HOSTNAME.log
+tar -czvf $LOG_LOCATION/bactes-$HOSTNAME.tar.gz $LOG_LOCATION/bactes-$HOSTNAME.log
 
-rm -f $LOG_LOCATION/bactest-$HOSTNAME.log
+rm -f $LOG_LOCATION/bactes-$HOSTNAME.log
 
 if [ ! $LOG_FILE $DEST_LOCATION/$YEAR/$MONTH ]; then
     mv $LOG_FILE $DEST_LOCATION/$YEAR/$MONTH ;
 fi;
 
-# chmod +x /root/log-test/backup.sh
+chmod +x /root/log-test/backup.sh
 
-# crontab -e 0 1 * * * /root/log-test/backup.sh
-
+crontab -e 0 1 * * * /root/log-test/backup.sh
