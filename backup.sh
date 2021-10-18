02a424e6e0ca35e931043681ae16a9f1435ee215
akselnuredin <akslnrdn@gmail.com>
2021-10-18 11:18:40 +0200

    Added $DAY variable

--- backup.sh
+++ backup.sh
@@ -4,6 +4,7 @@ echo "This script moves log file from a localhost to an S3 bucket."
 
 YEAR=$(date +"%Y")
 MONTH=$(date +"%m")
+DAY=$(date +"%d")
 HOSTNAME=$(hostname)
 CUSTOM=$(date +"%F")
 LOG_LOCATION='/opt/app/arkcase/log'
@@ -14,6 +15,7 @@ DEST_LOCATION='/sharecare-prod-s3fs/'
 if [[ ! -d /opt/$YEAR ]] || [[ ! -d /opt/$YEAR/$MONTH ]]; then
     mkdir -p /opt/$YEAR;
     mkdir -p /opt/$YEAR/$MONTH;
+    mkdir -p /opt/$YEAR/$MONTH/$DAY;
 fi;
 
 mv  $LOG_LOCATION/bactes-$CUSTOM.log $LOG_LOCATION/bactes-$HOSTNAME.log
@@ -22,8 +24,8 @@ tar -czvf $LOG_LOCATION/bactes-$HOSTNAME.tar.gz $LOG_LOCATION/bactes-$HOSTNAME.l
 
 rm -f $LOG_LOCATION/bactes-$HOSTNAME.log
 
-if [ ! $LOG_FILE $DEST_LOCATION/$YEAR/$MONTH ]; then
-    mv $LOG_FILE $DEST_LOCATION/$YEAR/$MONTH ;
+if [ ! $LOG_FILE $DEST_LOCATION/$YEAR/$MONTH/$DAY ]; then
+    mv $LOG_FILE $DEST_LOCATION/$YEAR/$MONTH/$DAY ;
 fi;
 
 chmod +x /root/log-test/backup.sh
